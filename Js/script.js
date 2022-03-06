c2434478226e97adf6f71c3c637a2f0324796cbe
Ivy Kabere <kabereivy@gmail.com>
2022-03-07 00:26:41 +0300

    add variables for daysOfWeek and Names

---
+++ Js/script.js
@@ -0,0 +1,20 @@
+const dayOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const maleNames = [
+  "Kwasi",
+  "Kwadwo",
+  "Kwabena",
+  "Kwaku",
+  "Yaw",
+  "Kofi",
+  "Kwame",
+];
+
+const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
